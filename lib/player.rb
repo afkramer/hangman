8dee4be29a800dfa0776f344303f71f048cca6d5
Adriana Kramer <adri.kramer@gmail.com>
2022-04-11 18:41:12 +0200

    Add code to Player class

---
+++ lib/player.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Player class contains methods for the human player
+
+class Player
+  attr_reader :name
+
+  def initialize
+    @name = ''
+  end
+
+  def get_guess
+    # Call the get_guess method of the Gui
+    # Check if it is a valid answer (a letter)
+      # If valid return the letter
+    # If not a valid answer
+      # Call the invalic input method of the Gui
+      # call the guess method of the Gui
+  end
+end 
