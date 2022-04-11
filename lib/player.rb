da6504536e22082c483c5e5ba21517593bfb6bc6
Adriana Kramer <adri.kramer@gmail.com>
2022-04-11 18:55:25 +0200

    Add Player#get_guess method

--- lib/player.rb
+++ lib/player.rb
@@ -7,14 +7,18 @@ class Player
 
   def initialize
     @name = ''
+    @guesses = []
+    @gui = Gui.new
   end
 
   def get_guess
-    # Call the get_guess method of the Gui
-    # Check if it is a valid answer (a letter)
-      # If valid return the letter
-    # If not a valid answer
-      # Call the invalic input method of the Gui
-      # call the guess method of the Gui
+    guess = gui.get_guess.downcase
+    if /[a-z]/.match?(guess) && guess.length == 1 && !@guesses.include?(guess)
+      @guesses << guess
+      guess
+    else
+      gui.invalid_input
+      get_guess
+    end
   end
-end 
+end
