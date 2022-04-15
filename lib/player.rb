77d3f351d69075c1209cb9bcfd22d652f9fe783b
Adriana Kramer <adri.kramer@gmail.com>
2022-04-15 07:56:17 +0200

    Add to_json and from_json methods to Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
+require 'json'
+require './lib/gui'  # remove after testing (BUT how did this work before???)
+
 # Player class contains methods for the human player
 class Player
-  attr_accessor :name, :lives_left, :games_won, :games_lost, :correct_guesses, :incorrect_guesses
+  attr_accessor :name, :correct_guesses, :incorrect_guesses,
+                :lives_left, :games_won, :games_lost
 
   def initialize
     @name = ''
@@ -12,6 +16,35 @@ class Player
     @lives_left = []
     @games_won = 0
     @games_lost = 0
+  end
+
+  def save_player
+    # write to a file
+  end
+
+  def load_player
+    # retrieve the player from the file
+  end
+
+  def to_json
+    JSON.dump({
+                name: @name,
+                incorrect_guesses: @incorrect_guesses,
+                correct_guesses: @correct_guesses,
+                lives_left: @lives_left,
+                games_won: @games_won,
+                games_lost: @games_lost
+              })
+  end
+
+  def from_json(string)
+    data = JSON.parse(string)
+    @name = data['name']
+    @incorrect_guesses = data['incorrect_guesses']
+    @correct_guesses = data['correct_guesses']
+    @lives_left = data['lives_left']
+    @games_won = data['games_won']
+    @games_lost = data['games_lost']
   end
 
   def reset_guesses
